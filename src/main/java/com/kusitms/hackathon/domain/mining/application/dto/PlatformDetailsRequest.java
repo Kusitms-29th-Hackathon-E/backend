e015db8b1170eb410ce4f758fa20f616299cb7f1
Sim-km <simkm0824@naver.com>
2024-04-07 02:44:41 +0900

    feat : dto 수정

--- src/main/java/com/kusitms/hackathon/domain/mining/application/dto/PlatformDetailsRequest.java
+++ src/main/java/com/kusitms/hackathon/domain/mining/application/dto/PlatformDetailsRequest.java
@@ -1,7 +1,12 @@
 package com.kusitms.hackathon.domain.mining.application.dto;
 
-public record PlatformDetailsRequest(
-        String platformUrl
-) {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PlatformDetailsRequest{
+    private String platformUrl;
 }
