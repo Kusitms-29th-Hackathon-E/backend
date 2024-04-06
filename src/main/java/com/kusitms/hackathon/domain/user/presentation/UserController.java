e1dafea4a0878cec01e27024456ede4fe866d8dd
Sim-km <simkm0824@naver.com>
2024-04-06 16:57:41 +0900

    feat : 스웨거 의존성 추가

--- src/main/java/com/kusitms/hackathon/domain/user/presentation/UserController.java
+++ src/main/java/com/kusitms/hackathon/domain/user/presentation/UserController.java
@@ -1,6 +1,9 @@
 package com.kusitms.hackathon.domain.user.presentation;
 
+import com.kusitms.hackathon.domain.user.application.service.UserService;
+import com.kusitms.hackathon.domain.user.domain.Provider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,12 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 public class UserController {
+    private UserService userService;
 
 
-    @PostMapping("/oauth")
+    @PostMapping("/oauth/{provider}")
     public void processingOAuth(
-        @RequestHeader("OAuth-AccessToken") String accessToken
-    ){
-
+            @RequestHeader("OAuth-AccessToken") String accessToken,
+            @PathVariable Provider provider
+    ) {
+        userService.oAuthExecuting(accessToken, provider);
     }
 }
